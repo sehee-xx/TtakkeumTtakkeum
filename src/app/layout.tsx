6ebefcdebd94eb9e458f33fe60d24b4a6306f88d
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-14 22:48:32 +0900

    Modify: 모든 페이지 로딩 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import GlobalStyle from "../../styles/GlobalStyles";
+import Loading from "@/components/Loading";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleComplete = () => setLoading(false);
+
+    if (document.readyState === "complete") {
+      setLoading(false);
+    } else {
+      window.addEventListener("load", handleComplete);
+    }
+
+    return () => {
+      window.removeEventListener("load", handleComplete);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <GlobalStyle />
-      <body>{children}</body>
+      <body>{loading ? <Loading /> : children}</body>
     </html>
   );
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -15,13 +15,15 @@ const LoadingWrapper = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  font-size: 30px;
 `;
 
 const LoadingImg = styled.img`
   width: 100px;
   height: 100px;
   object-fit: cover;
+  padding-bottom: 30px;
 `;
